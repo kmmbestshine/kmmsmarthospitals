b2b7fb771182078d9dabc8201c2b0e1583aebf3e
example1 <32572574+bestshine@users.noreply.github.com>
2020-08-20 20:37:31 +0530

    initial commit
    initial commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AttendancesTableSeeder::class);
+        //$this->call(AttendancesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(MedicinesTableSeeder::class);
-        $this->call(PatientTableSeeder::class);
-        $this->call(WardTableSeeder::class);
-        $this->call(ClinicsTableSeeder::class);
-        $this->call(NoticeboardsTableSeeder::class);
+        //$this->call(MedicinesTableSeeder::class);
+        //$this->call(PatientTableSeeder::class);
+        //$this->call(WardTableSeeder::class);
+        //$this->call(ClinicsTableSeeder::class);
+        //$this->call(NoticeboardsTableSeeder::class);
     }
 }
